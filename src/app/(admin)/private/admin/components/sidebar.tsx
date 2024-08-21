697155f448b3f65a0d4541f27c08b9747de50a1c
CrisCorzo97 <crcorzo04@hotmail.com>
2024-08-21 15:58:24 -0300

    update sidebar

--- src/app/(admin)/private/admin/components/sidebar.tsx
+++ src/app/(admin)/private/admin/components/sidebar.tsx
@@ -1,10 +1,25 @@
 import { LogoRD, LogoRents } from '@/assets/icons';
-import { ChevronRight, LayoutDashboard } from 'lucide-react';
+import { cn } from '@/lib/cn';
+import { Building2, ChevronRight, Church, LayoutDashboard } from 'lucide-react';
 import Link from 'next/link';
+
+const sidebarListStyles = {
+  li: {
+    className: 'list-none h-12 mt-2 flex items-center',
+    a: {
+      className:
+        'h-full w-full flex items-center rounded transition hover:bg-primary hover:text-primary-foreground',
+      icon: 'w-5 h-5',
+    },
+  },
+};
 
 export const Sidebar = () => {
   return (
-    <nav className='fixed w-56 top-0 left-0 h-screen bg-secondary'>
+    <nav
+      className={cn('fixed w-56 top-0 left-0 h-screen bg-secondary', 'w-20')}
+    >
+      {/* Logo Section */}
       <header className='relative'>
         <div className='flex items-center'>
           <span className='min-w-14 flex items-center justify-center'>
@@ -21,15 +36,37 @@ export const Sidebar = () => {
         </span>
       </header>
 
-      <section>
+      {/* Sidebar Section */}
+      <section className='h-full flex flex-col justify-between'>
         <div>
           <ul>
-            <li className='list-none h-12 mt-2 flex items-center'>
-              <Link
-                href='#'
-                className='h-full w-full flex items-center rounded transition hover:bg-primary'
-              >
-                <LayoutDashboard className='text-xl min-w-14 flex items-center justify-center' />
+            <li className={sidebarListStyles.li.className}>
+              <Link href='#' className={sidebarListStyles.li.a.className}>
+                <LayoutDashboard className={sidebarListStyles.li.a.icon} />
+                <span>Tablero</span>
+              </Link>
+            </li>
+            <li className={sidebarListStyles.li.className}>
+              <Link href='#' className={sidebarListStyles.li.a.className}>
+                <Building2 className={sidebarListStyles.li.a.icon} />
+                <span>Inmueble</span>
+              </Link>
+            </li>
+            <li className={sidebarListStyles.li.className}>
+              <Link href='#' className={sidebarListStyles.li.a.className}>
+                <Church className={sidebarListStyles.li.a.icon} />
+                <span>Cementerio</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Sidebar Bottom Section */}
+        <div>
+          <ul>
+            <li className={sidebarListStyles.li.className}>
+              <Link href='#' className={sidebarListStyles.li.a.className}>
+                <LayoutDashboard className={sidebarListStyles.li.a.icon} />
                 <span>Tablero</span>
               </Link>
             </li>
