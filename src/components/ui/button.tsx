bb7910c1b46eea4de5f5dbf8f1843342746c1d23
CrisCorzo97 <crcorzo04@hotmail.com>
2024-09-20 11:22:39 -0300

    modifiy button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 
 import { cn } from '@/lib/cn';
 import { Slot } from '@radix-ui/react-slot';
+import { Loader2 } from 'lucide-react';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -42,17 +43,39 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, loading, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
     return (
-      <Comp
-        className={cn(
-          buttonVariants({ variant, size, className }),
-          'transition-all hover:opacity-90',
-          loading && 'flex gap-1 opacity-80'
+      <>
+        {asChild ? (
+          <Slot
+            ref={ref}
+            className={cn(
+              buttonVariants({ variant, size, className }),
+              'transition-all hover:opacity-90',
+              loading && 'flex gap-1 opacity-80'
+            )}
+            disabled={loading}
+            {...props}
+          ></Slot>
+        ) : (
+          <button
+            ref={ref}
+            className={cn(
+              buttonVariants({ variant, size, className }),
+              'transition-all hover:opacity-90',
+              loading && 'flex gap-1 opacity-80'
+            )}
+            disabled={loading}
+            {...props}
+          >
+            {loading ? (
+              <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+            ) : (
+              <></>
+            )}
+            {props.children}
+          </button>
         )}
-        ref={ref}
-        {...props}
-      />
+      </>
     );
   }
 );
