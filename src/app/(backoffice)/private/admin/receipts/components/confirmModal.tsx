f82721c2b6f3893a1f7eea601bd2098d9cfd0034
CrisCorzo97 <crcorzo04@hotmail.com>
2024-10-02 10:04:36 -0300

    Refactor confirmModal component and add receipt search functionality

--- src/app/(backoffice)/private/admin/receipts/components/confirmModal.tsx
+++ src/app/(backoffice)/private/admin/receipts/components/confirmModal.tsx
@@ -8,20 +8,48 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { receipt } from '@prisma/client';
-import { CircleCheckBig } from 'lucide-react';
+import dayjs from 'dayjs';
+import { CircleCheckBig, Search } from 'lucide-react';
 import { useState, useTransition } from 'react';
 import { getReceiptById } from '../receipt-actions';
 
 export function ConfirmModal() {
   const [receiptData, setReceiptData] = useState<receipt | null>(null);
+  const [error, setError] = useState<string>('');
   const [isSearching, startTransition] = useTransition();
+  const [isMutating, startMutation] = useTransition();
 
   const handleSearch = (formData: FormData) => {
     startTransition(async () => {
-      const receiptId = formData.get('receipt_number') as string;
+      const receiptId = formData.get('receipt_id') as string;
+
+      try {
+        const receipt = await getReceiptById({
+          id: receiptId,
+        });
+
+        if (receipt.data) {
+          setError('');
+          return setReceiptData(receipt.data);
+        }
+
+        return setError(
+          'No se encontró el comprobante de pago, intenta nuevamente.'
+        );
+      } catch (error) {
+        console.error(error);
+        setError('No se encontró el comprobante de pago, intenta nuevamente.');
+      }
+    });
+  };
+
+  const handleSubmit = (formData: FormData) => {
+    startMutation(async () => {
+      const receiptId = formData.get('receipt_id') as string;
 
       try {
         const receipt = await getReceiptById({
@@ -33,33 +61,92 @@ export function ConfirmModal() {
         }
       } catch (error) {
         console.error(error);
+        setError('No se encontró el comprobante de pago, intenta nuevamente.');
       }
     });
   };
 
   return (
-    <Dialog>
+    <Dialog onOpenChange={() => setReceiptData(null)}>
       <DialogTrigger asChild>
         <Button size='lg' variant='outline' className='flex gap-2'>
           <CircleCheckBig size={18} />
           Confirmar
         </Button>
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[425px]'>
-        <form action={handleSearch}>
-          <DialogHeader>
-            <DialogTitle>Confirmar el pago de un comprobante</DialogTitle>
-            <DialogDescription>
-              Ingresa el número del comprobante para confirmar el pago.
-            </DialogDescription>
-          </DialogHeader>
-          <div className='flex flex-col gap-3 my-6'>
-            <Label htmlFor='receipt_number'>Comprobante N°:</Label>
-            <Input type='number' id='receipt_number' placeholder='0000000000' />
-          </div>
+      <DialogContent className='sm:max-w-lg'>
+        <DialogHeader>
+          <DialogTitle>Confirmar el pago de un comprobante</DialogTitle>
+          <DialogDescription>
+            Ingresa el número del comprobante para confirmar el pago.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className='flex flex-col mt-6 '>
+          <form action={handleSearch}>
+            <FormItem>
+              <Label htmlFor='receipt_id'>Comprobante N°:</Label>
+
+              <div className='flex'>
+                <Input
+                  type='text'
+                  name='receipt_id'
+                  placeholder='0000000000'
+                  required
+                />
+                <Button type='submit' loading={isSearching}>
+                  <Search size={24} color='#FFF' />
+                </Button>
+              </div>
+              {!!error && <p className='text-red-500 text-sm'>{error}</p>}
+            </FormItem>
+          </form>
+        </div>
+
+        <form action={handleSubmit}>
+          {receiptData && (
+            <div className='flex flex-col gap-3 mb-6'>
+              <FormItem>
+                <Label>Contribuyente</Label>
+                <Input
+                  type='text'
+                  value={receiptData.taxpayer}
+                  readOnly
+                  className='cursor-not-allowed'
+                />
+              </FormItem>
+
+              <div className='flex gap-2'>
+                <FormItem>
+                  <Label>Fecha de emisión</Label>
+                  <Input
+                    type='text'
+                    value={dayjs(receiptData.created_at).format(
+                      'DD/MM/YYYY HH:mm'
+                    )}
+                    readOnly
+                    className='cursor-not-allowed'
+                  />
+                </FormItem>
+                <FormItem>
+                  <Label>Monto:</Label>
+                  <Input
+                    type='text'
+                    value={receiptData.amount.toLocaleString('es-AR', {
+                      style: 'currency',
+                      currency: 'ARS',
+                      maximumFractionDigits: 2,
+                    })}
+                    readOnly
+                    className='cursor-not-allowed'
+                  />
+                </FormItem>
+              </div>
+            </div>
+          )}
 
           <DialogFooter>
-            <Button type='submit' loading={isSearching}>
+            <Button type='submit' loading={isMutating} disabled={!receiptData}>
               Confirmar
             </Button>
           </DialogFooter>
