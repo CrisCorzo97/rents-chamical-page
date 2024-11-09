8872092cf1cfac22387bbefb38eeb4022460c24b
CrisCorzo97 <crcorzo04@hotmail.com>
2024-11-09 14:32:39 -0300

    Add Vercel Analytics integration to layout and update package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from '@/components/ui/toaster';
 import StyledComponentsRegistry from '@/lib/ant-design/AntdRegistry';
 import theme from '@/theme/themeConfig';
+import { Analytics } from '@vercel/analytics/react';
 import { ConfigProvider } from 'antd';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -39,6 +40,7 @@ export default function RootLayout({
             <Toaster />
           </ConfigProvider>
         </StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   );
