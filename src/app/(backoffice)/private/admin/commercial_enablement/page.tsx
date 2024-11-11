5e801201bef5b0e29e27428ca972af3de989516d
CrisCorzo97 <crcorzo04@hotmail.com>
2024-11-11 10:53:07 -0300

    Add Commercial Enablement page and update sidebar navigation

---
+++ src/app/(backoffice)/private/admin/commercial_enablement/page.tsx
@@ -0,0 +1,46 @@
+import { WorkInProgressIllustration } from '@/assets/illustrations';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import Link from 'next/link';
+
+export default function CommercialEnablement() {
+  return (
+    <ScrollArea className='mx-6 h-admin-scroll-area'>
+      <Breadcrumb className='h-12 mt-6'>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href='/' prefetch>
+                Inicio
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>Portal Administrativo</BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Habilitación comercial</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className='flex flex-col items-center justify-center h-full'>
+        <WorkInProgressIllustration className='w-80 h-80 mx-auto mt-12' />
+        <h1 className='text-2xl font-bold text-center mt-6'>
+          ¡Estamos trabajando en esta página!
+        </h1>
+        <p className='text-center mt-2'>
+          Próximamente podrás disfrutar de todas las funcionalidades de esta
+          sección.
+        </p>
+      </div>
+    </ScrollArea>
+  );
+}
