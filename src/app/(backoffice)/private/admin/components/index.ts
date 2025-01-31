515e18dd241c038ac610f923b81e669edf60d47d
CrisCorzo97 <crcorzo04@hotmail.com>
2025-01-30 22:28:52 -0300

    Remove unused exports for CitySectionTable and NeighborhoodTable from admin components index

--- src/app/(backoffice)/private/admin/components/index.ts
+++ src/app/(backoffice)/private/admin/components/index.ts
@@ -1,3 +1,1 @@
-export { CitySectionTable } from './city-section-table/citySectionTable';
-export { NeighborhoodTable } from './neighborhood-table/neighborhoodTable';
 export { Sidebar } from './sidebar/sidebar';
