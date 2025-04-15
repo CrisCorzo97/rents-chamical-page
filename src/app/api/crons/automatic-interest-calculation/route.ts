d049172db7d80ed71978fac9efb67060cf5cef67
CrisCorzo97 <crcorzo04@hotmail.com>
2025-04-15 20:55:17 -0300

    Reducir el número de facturas procesadas en el cron de cálculo de intereses automáticos de 100 a 50.

--- src/app/api/crons/automatic-interest-calculation/route.ts
+++ src/app/api/crons/automatic-interest-calculation/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: Request) {
             lt: dayjs().startOf('day').toDate(),
           },
         },
-        take: 100,
+        take: 50,
         skip: invoices.length,
       });
 
